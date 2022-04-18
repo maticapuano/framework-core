a4486be5695fc05af0ef821fc14c89c8977f4a3a
Matias Capuano <maticapuano97@gmail.com>
2022-04-17 23:57:58 -0300

    chore(utils): improve remove undefind object or arrays

--- src/utils/shared.util.ts
+++ src/utils/shared.util.ts
@@ -1,18 +1,25 @@
 export const isString = (value: any): value is string => typeof value === "string";
 export const isObject = (value: any): value is object =>
     typeof value === "object" && value !== null;
+export const isArray = (value: any): value is Array<any> => Array.isArray(value);
 
-export const removeUndefined = <T = Object>(value: T): T => {
-    if (!isObject(value)) {
-        return value;
+export const removeUndefined = <T = Object>(value: T | T[]): T[] => {
+    const result: any[] = [];
+
+    if (isArray(value)) {
+        result.push(...value.filter(item => item !== undefined));
     }
 
-    const result: any = {};
+    if (isObject(value) && !isArray(value)) {
+        const filtered = Object.keys(value).reduce<Record<string, T>>((acc, key) => {
+            if (value[key] !== undefined) {
+                acc[key] = value[key];
+            }
 
-    for (const key in value) {
-        if (value[key] !== undefined) {
-            result[key] = value[key];
-        }
+            return acc;
+        }, {});
+
+        result.push(filtered);
     }
 
     return result;
