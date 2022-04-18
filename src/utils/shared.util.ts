ff4b1f887ffff85520da8ed8da17a2be3b23deca
Matias Capuano <maticapuano97@gmail.com>
2022-04-18 11:03:33 -0300

    chore(utils): scan directory from path

--- src/utils/shared.util.ts
+++ src/utils/shared.util.ts
@@ -1,7 +1,10 @@
+import { sync } from "glob";
+
 export const isString = (value: any): value is string => typeof value === "string";
 export const isObject = (value: any): value is object =>
     typeof value === "object" && value !== null;
 export const isArray = (value: any): value is Array<any> => Array.isArray(value);
+export const castArray = <T = any>(value: any): Array<T> => (isArray(value) ? value : [value]);
 
 export const removeUndefined = <T = Object>(value: T | T[]): T[] => {
     const result: any[] = [];
@@ -23,4 +26,28 @@ export const removeUndefined = <T = Object>(value: T | T[]): T[] => {
     }
 
     return result;
+};
+
+export const getSourceFiles = (files: string | Array<string>): string[] => {
+    const result: Set<string> = new Set<string>();
+    const options = {
+        cwd: process.cwd(),
+        ignore: [
+            "**/node_modules/**",
+            "**/dist/**",
+            "**/test/**",
+            "**/__tests__/**",
+            "**/__mocks__/**",
+            "**/__test__/**",
+        ],
+    };
+    const filesExpression = castArray<string>(files);
+
+    filesExpression.forEach(file => {
+        const matches = sync(file, options);
+
+        matches.forEach(match => result.add(match));
+    });
+
+    return Array.from(result);
 };
