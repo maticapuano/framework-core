2943bda15e262669bc70263117ccd866452d2f40
Matias Capuano <maticapuano97@gmail.com>
2022-04-22 19:43:04 -0300

    chore(utils/serialzie-path): set path lower case

--- src/utils/serialize-path.ts
+++ src/utils/serialize-path.ts
@@ -1,7 +1,8 @@
 export function serializePath(path: string): string;
 export function serializePath(path: string, withSeparator: boolean): string;
 export function serializePath(path: string, withSeparator?: boolean): string {
-    const pathArray = path.split("/").filter(Boolean);
+    const pathLower = path.toLowerCase();
+    const pathArray = pathLower.split("/").filter(Boolean);
     const pathSerialized = pathArray.join("/");
     const pathSerializedWithSlash = `/${pathSerialized}`;
 
