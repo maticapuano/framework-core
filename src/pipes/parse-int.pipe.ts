bd7f419ef5388fdab87b20cc23e0aa889e8b3235
Matias Capuano <maticapuano97@gmail.com>
2022-06-19 20:51:41 -0300

    fix(pipes): fix exception

--- src/pipes/parse-int.pipe.ts
+++ src/pipes/parse-int.pipe.ts
@@ -1,10 +1,12 @@
-import { BadGatewayException } from "@exceptions/http/bad-gateway.exception";
+import { BadRequestException } from "@exceptions/http/bad-request.exception";
 import { PipeTransform } from "@interfaces/pipes/pipe-transform";
+import { Injectable } from "../decorators/injectable.decorator";
 
+@Injectable()
 export class ParseIntPipe implements PipeTransform {
     public transform(value: string): number {
         if (!this.isNumericValue(value)) {
-            throw new BadGatewayException(`${value} is not a numeric value`);
+            throw new BadRequestException(`${value} is not a numeric value`);
         }
 
         const parsedValue = parseInt(value, 10);
